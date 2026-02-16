df0b1d738f8a5320bcf50efd342977933d39c920
eityroy <eityroy027@gmail.com>
2026-02-16 21:20:52 +0600

    react project2

--- react-second-project/src/component/Bottle/Bottle.jsx
+++ react-second-project/src/component/Bottle/Bottle.jsx
@@ -3,10 +3,17 @@ import './Bottle.css';
 
 const Bottle = ({bottle}) => {
     console.log(bottle);
-    const { img } = bottle; //using destructure
+    const { img, name,seller,price,ratings,stock,shipping} = bottle; //using destructure
     return (
         <div className='card'>
     <img style={{width:"300px",height:"300px"}} src= {img} alt="" />
+    <h3>Name: {name}</h3>
+    <h3>Seller:{seller}</h3>
+    <h3>Price: {price}</h3>
+    <h3>Ratings: {ratings}</h3>
+    <h3>Stock:{stock}</h3>
+    <h3>Shipping:{shipping}</h3>
+    
         </div>
     );
 };
