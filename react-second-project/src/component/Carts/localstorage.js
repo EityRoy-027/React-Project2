365e4c09491b9db4515773af20f0184c3e7d7a9f
eityroy <eityroy027@gmail.com>
2026-02-17 02:27:58 +0600

    react project2

---
+++ react-second-project/src/component/Carts/localstorage.js
@@ -0,0 +1,22 @@
+const getCartFromLocalStorage = () =>{
+    const storedItem= localStorage.getItem( 'cart' );
+
+    if(storedCartstring){
+        const storedcart= JSON.parse(storedCartstring)
+        return storedcart;
+    }
+    return [];
+}
+
+
+const saveCartToLocalStorage = cart =>{
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart' , cartStringified);
+}
+
+const addItemToCartLocalStorage = id => {
+    const cart = getCartFromLocalStorage();
+    cart.push(id);
+    
+    saveCartToLocalStorage(newcart);
+}
