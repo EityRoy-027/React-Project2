70fb90d31d540157c7d9cadee7721c9baa6a9a9d
eityroy <eityroy027@gmail.com>
2026-02-16 11:38:17 +0600

    react poject2

--- react-second-project/src/component/Bottles/Bottles.jsx
+++ react-second-project/src/component/Bottles/Bottles.jsx
@@ -1,10 +1,12 @@
 import React, { use } from 'react';
+import Bottle from '../Bottle/Bottle';
+import './bottles.css';
 
 const Bottles = ({bottlesPromise}) => {
     const bottles=use(bottlesPromise);
     console.log(bottles);
     return (
-        <div>
+        <div className='Bottles-container'>
             <h3>bottles: {bottles.length}</h3>
             {
                 bottles.map(bottle=> <Bottle key={bottle.id} bottle={bottle}></Bottle>)
